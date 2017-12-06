c7fe5677909fdcba98ac9b840eca599302ebeea9
Florent Radiguet <florent.radiguet@precurseur.fr>
2017-12-06 17:07:15 +0100

    style ci fix

--- DependencyInjection/CompilerPass/RemoveTaggedServiceCollectorCompilerPass.php
+++ DependencyInjection/CompilerPass/RemoveTaggedServiceCollectorCompilerPass.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Sulu\Bundle\FormBundle\DependencyInjection\CompilerPass;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
@@ -42,9 +52,8 @@ class RemoveTaggedServiceCollectorCompilerPass implements CompilerPassInterface
         $disabledSerivcesAliases = $container->getParameter($this->disableParam);
         $taggedServices = $container->findTaggedServiceIds($this->tagName);
 
-        foreach($taggedServices as $id => $attributes) {
-
-            if(isset($attributes[0][$this->aliasAttribute]) 
+        foreach ($taggedServices as $id => $attributes) {
+            if (isset($attributes[0][$this->aliasAttribute])
                     && in_array($attributes[0][$this->aliasAttribute], $disabledSerivcesAliases)) {
                 if ($container->hasDefinition($id)) {
                     $container->getDefinition($id)->setTags([]);
