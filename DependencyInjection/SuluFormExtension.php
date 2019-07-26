db9aa8172c2bf9af916a2befb38dcfb1a141bcd7
Markus Moosbrugger <markus.l.moosbrugger@student.uibk.ac.at>
2019-07-26 13:32:06 +0200

    Added delete and export toolbar action

--- DependencyInjection/SuluFormExtension.php
+++ DependencyInjection/SuluFormExtension.php
@@ -77,6 +77,7 @@ class SuluFormExtension extends Extension implements PrependExtensionInterface
                         'dynamics' => [
                             'routes' => [
                                 'list' =>'sulu_form.get_dynamics',
+                                'detail' => 'sulu_form.delete_dynamic',
                             ],
                         ]
                     ],
