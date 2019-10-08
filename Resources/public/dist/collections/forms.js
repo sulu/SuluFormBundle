d7bb71f3e2ebb6d2623d39acd3cf827c6ef8ab58
Alexander Schranz <alexander@sulu.io>
2019-10-08 14:25:53 +0200

    Remove old frontend code

--- Resources/public/dist/collections/forms.js
+++
@@ -1,1 +0,0 @@
-define(["mvc/collection","suluform/model/form"],function(a,b){return a({model:b,url:function(){return"/admin/api/forms"},fieldsUrl:function(){return"/admin/api/forms/fields"},parse:function(a){return a._embedded.forms}})});
--- Resources/public/dist/components/csv-export/main.js
+++
@@ -1,1 +0,0 @@
-define(["text!/admin/api/form/templates/csv-export.html"],function(a){"use strict";return{type:"csv-export",getFormTemplate:function(){return a}}});
