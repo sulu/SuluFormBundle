43dfcf1035f19728658b865ead0b93055150fcbf
Martin Lagler <martin.lagler@MBP0069.local>
2020-02-26 17:25:43 +0100

    Remove required from Recaptcha

--- Dynamic/Types/RecaptchaType.php
+++ Dynamic/Types/RecaptchaType.php
@@ -30,7 +30,7 @@ class RecaptchaType implements FormFieldTypeInterface
     {
         return new FormFieldTypeConfiguration(
             'sulu_form.type.recaptcha',
-            __DIR__ . '/../../Resources/config/form-fields/default_field.xml',
+            __DIR__ . '/../../Resources/config/form-fields/field_recaptcha.xml',
             [],
             'special'
         );
