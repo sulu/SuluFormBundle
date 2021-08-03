f7a6f8660181ba8fea7e273576fabaa466b08687
nibressler <88328175+nibressler@users.noreply.github.com>
2021-08-03 10:39:10 +0200

    Add files via upload
    create phone number validation

---
+++ PhoneType.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) Sulu GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\FormBundle\Dynamic\Types;
+
+use Sulu\Bundle\FormBundle\Dynamic\FormFieldTypeConfiguration;
+use Sulu\Bundle\FormBundle\Dynamic\FormFieldTypeInterface;
+use Sulu\Bundle\FormBundle\Entity\FormField;
+use Symfony\Component\Form\Extension\Core\Type\TelType as TypeTelType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * The Phone form field type.
+ */
+class PhoneType implements FormFieldTypeInterface
+{
+    use SimpleTypeTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfiguration(): FormFieldTypeConfiguration
+    {
+        return new FormFieldTypeConfiguration(
+            'sulu_form.type.phone',
+            __DIR__ . '/../../Resources/config/form-fields/field_example_default.xml'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build(FormBuilderInterface $builder, FormField $field, string $locale, array $options): void
+    {
+        $options['constraints'] = [new Callback(function($object, ExecutionContextInterface $context, $payload) {
+
+            $exception = false;
+
+            $phoneUtil = \libphonenumber\PhoneNumberUtil::getInstance();
+            try {
+                $swissNumberProto = $phoneUtil->parse($object, "DE");
+                $exception = !$phoneUtil->isValidNumber($swissNumberProto);
+
+            } catch (\libphonenumber\NumberParseException $e) {
+                $exception = true;
+            }
+
+            if ($exception) {
+                $context->buildViolation('Invalid phone number')
+                    ->addViolation()
+                ;
+            }
+        })];
+        $type = TypeTelType::class;
+        $builder
+            ->add($field->getKey(), $type, $options);
+    }
+}
