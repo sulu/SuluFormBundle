d07b4be64d8b8712ec9b2aac02374316e3db1196
Arson RAHERINIAINA <a.raheriniaina@gmail.com>
2023-04-07 16:08:43 +0300

    Add support of recaptcha v3

--- DependencyInjection/SuluFormExtension.php
+++ DependencyInjection/SuluFormExtension.php
@@ -13,6 +13,8 @@ namespace Sulu\Bundle\FormBundle\DependencyInjection;
 
 use Sulu\Bundle\FormBundle\Controller\FormTokenController;
 use Sulu\Bundle\FormBundle\Controller\FormWebsiteController;
+use Sulu\Bundle\FormBundle\Dynamic\Types\RecaptchaType;
+use Sulu\Bundle\FormBundle\Dynamic\Types\RecaptchaV3Type;
 use Sulu\Bundle\FormBundle\Entity\Form;
 use Sulu\Component\HttpKernel\SuluKernel;
 use Symfony\Component\Config\FileLocator;
@@ -223,6 +225,11 @@ class SuluFormExtension extends Extension implements PrependExtensionInterface
         }
 
         if (\class_exists(\EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType::class)) {
+            if (3 == $container->getParameter('ewz_recaptcha.version')) {
+                $container->setParameter('recaptcha_type.class', RecaptchaV3Type::class);
+            } else {
+                $container->setParameter('recaptcha_type.class', RecaptchaType::class);
+            }
             $loader->load('type_recaptcha.xml');
         }
 
