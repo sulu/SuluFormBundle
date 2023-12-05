72d447b23fd31665a3ae46b594c9e0e30eb878dc
Arson RAHERINIAINA <a.raheriniaina@gmail.com>
2023-12-06 00:07:48 +0300

    Fix  non-existent parameter ewz_recaptcha.version error

--- DependencyInjection/SuluFormExtension.php
+++ DependencyInjection/SuluFormExtension.php
@@ -225,12 +225,15 @@ class SuluFormExtension extends Extension implements PrependExtensionInterface
         }
 
         if (\class_exists(\EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType::class)) {
-            if (3 == $container->getParameter('ewz_recaptcha.version')) {
-                $container->setParameter('recaptcha_type.class', RecaptchaV3Type::class);
-            } else {
-                $container->setParameter('recaptcha_type.class', RecaptchaType::class);
+            // EWZRecaptchaBundle has to be registered before SuluFormBundle
+            if ($container->hasParameter('ewz_recaptcha.version')) {
+                if (3 == $container->getParameter('ewz_recaptcha.version')) {
+                    $container->setParameter('recaptcha_type.class', RecaptchaV3Type::class);
+                } else {
+                    $container->setParameter('recaptcha_type.class', RecaptchaType::class);
+                }
+                $loader->load('type_recaptcha.xml');
             }
-            $loader->load('type_recaptcha.xml');
         }
 
         if (SuluKernel::CONTEXT_WEBSITE === $container->getParameter('sulu.context')) {
