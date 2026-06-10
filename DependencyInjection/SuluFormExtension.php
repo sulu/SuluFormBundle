1d138f673eb2c0e0ad4629abe01a1e2ca4203a41
Prokyonn <daniel@sulu.io>
2026-06-10 13:40:05 +0200

    Remove dead esi config

--- DependencyInjection/SuluFormExtension.php
+++ DependencyInjection/SuluFormExtension.php
@@ -52,17 +52,6 @@ class SuluFormExtension extends Extension implements PrependExtensionInterface
             );
         }
 
-        if ($container->hasExtension('framework')) {
-            $container->prependExtensionConfig(
-                'framework',
-                [
-                    'esi' => [
-                        'enabled' => true,
-                    ],
-                ]
-            );
-        }
-
         if ($container->hasExtension('sulu_media')) {
             $container->prependExtensionConfig(
                 'sulu_media',
