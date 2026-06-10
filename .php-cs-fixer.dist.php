b6dad062fb9bfd959f26ebf4d15eabcbca93f7fd
Prokyonn <daniel.mathis@sector8.eu>
2026-06-10 16:50:46 +0200

    Update linting tools to be consistent with sulu/sulu 3.0 (#436)

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -15,6 +15,7 @@ $finder = PhpCsFixer\Finder::create()
 
 $config = new PhpCsFixer\Config();
 $config->setRiskyAllowed(true)
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRules([
         '@Symfony' => true,
         'array_syntax' => ['syntax' => 'short'],
@@ -25,16 +26,28 @@ $config->setRiskyAllowed(true)
         'native_constant_invocation' => true,
         'native_function_casing' => true,
         'native_function_invocation' => ['include' => ['@internal']],
+        'global_namespace_import' => ['import_classes' => false, 'import_constants' => false, 'import_functions' => false],
         'no_superfluous_phpdoc_tags' => ['allow_mixed' => true, 'remove_inheritdoc' => true],
         'ordered_imports' => true,
         'phpdoc_align' => ['align' => 'left'],
         'phpdoc_types_order' => false,
         'single_line_throw' => false,
         'single_line_comment_spacing' => false,
-        'trailing_comma_in_multiline' => false,
         'phpdoc_to_comment' => [
             'ignored_tags' => ['todo', 'var', 'see'],
         ],
+        'phpdoc_separation' => [
+            'groups' => [
+                ['Serializer\\*', 'VirtualProperty', 'Accessor', 'Type', 'Groups', 'Expose', 'Exclude', 'SerializedName', 'Inline', 'ExclusionPolicy'],
+            ],
+        ],
+        'get_class_to_class_keyword' => false, // should be enabled as soon as support for php < 8 is dropped
+        'nullable_type_declaration_for_default_null_value' => true,
+        'no_null_property_initialization' => false,
+        'fully_qualified_strict_types' => false,
+        'new_with_parentheses' => true,
+        'trailing_comma_in_multiline' => ['after_heredoc' => true, 'elements' => ['array_destructuring', 'arrays', 'match']],
+        'no_useless_else' => false,
     ])
     ->setFinder($finder);
 
